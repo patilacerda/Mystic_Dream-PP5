6f0d2dddf8b13111354852ee9eff70adfc493cb3
Patricia Rodrigues De Lacerda <patriciarlacerda@live.com>
2024-06-17 16:24:52 +0000

    Added checkout views, urls and template

--- mystic_dream/settings.py
+++ mystic_dream/settings.py
@@ -49,6 +49,10 @@ INSTALLED_APPS = [
     "products",
     "bag",
     "checkout",
+
+    #Other
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +69,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "mystic_dream.urls"
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -79,7 +86,12 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.media",
                 "bag.contexts.bag_contents",
+            ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
             ],
         },
     },
