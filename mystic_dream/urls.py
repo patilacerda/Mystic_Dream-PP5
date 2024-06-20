ce7878ba0992608658a4ed4a5613c796b5c5c1a5
Patricia Rodrigues De Lacerda <patriciarlacerda@live.com>
2024-06-20 03:48:53 +0000

    Added footer and newsletter input

--- mystic_dream/urls.py
+++ mystic_dream/urls.py
@@ -31,4 +31,4 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = 'mystic_dream.views.handler404'
+handler404 = handler404
