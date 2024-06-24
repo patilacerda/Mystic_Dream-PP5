14c22078db575ab2751f06ef3fb275c138075a71
Patricia Rodrigues De Lacerda <patriciarlacerda@live.com>
2024-06-24 03:56:36 +0000

    Fix indentation on profiles app with pep8

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,7 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
-    path('order_history/<order_number>', views.order_history, name='order_history'),
+    path(
+        'order_history/<order_number>',
+        views.order_history, name='order_history'
+        ),
     path('wishlist/', views.view_wishlist, name='view_wishlist'),
-    path('add_to_wishlist/<int:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
+    path(
+        'add_to_wishlist/<int:product_id>/',
+        views.add_to_wishlist, name='add_to_wishlist'
+        ),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -21,7 +21,8 @@ def profile(request):
             form.save()
             messages.success(request, 'Profile updated successfully')
         else:
-            messages.error(request, 'Update failed. Please ensure the form is valid.')
+            messages.error(
+                request, 'Update failed. Please ensure the form is valid.')
     else:
         form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
@@ -58,7 +59,8 @@ def order_history(request, order_number):
 def add_to_wishlist(request, product_id):
     """Add a product to the user's wishlist"""
     product = get_object_or_404(Product, pk=product_id)
-    wishlist, created = Wishlist.objects.get_or_create(user=request.user, product=product)
+    wishlist, created = Wishlist.objects.get_or_create(
+        user=request.user, product=product)
     if created:
         messages.success(request, f'Added {product.name} to your wishlist!')
     else:
