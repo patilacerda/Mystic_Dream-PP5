cb0c3fca367f0d26162a2f51ce21638178bd23d5
Patricia Rodrigues De Lacerda <patriciarlacerda@live.com>
2024-06-25 08:00:38 +0000

    Added contact, privacy and faq templates, views and urls

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path("", views.index, name='home'),
+    path("contact/", views.contact_page, name='contactus'),
+    path("faq/", views.faq_page, name='faq'),
+    path("privacy/", views.privacy_page, name='privacy'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,7 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def contact_page(request):
+    ''' Renders contact page  '''
+
+    return render(request, 'home/contactus.html')
+
+
+def faq_page(request):
+    ''' Renders FAQ page  '''
+
+    return render(request, 'home/faq.html')
+
+
+def privacy_page(request):
+    ''' Renders privacy page  '''
+
+    return render(request, 'home/privacy.html')
